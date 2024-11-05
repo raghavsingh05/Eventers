4634c0d56aa022b2dc70848639b5425238c80aa0
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-05 23:15:54 +0530

    header:initials

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link"
+
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href={"/"} className="w-36">
+        <Image src="" /></Link>
+      </div>
+    </header>
   )
 }
 
