c2d330a6691c8427dd3f2084c3a0281a342cfb24
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-08 16:20:15 +0530

    Add feature: added hero section data and styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,33 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10 ">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8 ">
+            <h1 className=" h1-bold ">Host with Ease, Connect with Guests: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Eventers simplifies event planning with easy guest management, QR check-ins, and real-time tracking</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+          <Image src="/assets/images/hero.png" alt="hero Image" height={1000} width={1000} className="max-h-[70vh] object-contain object-center 3xl:max-h-[50vh]"></Image>
+        </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12" >
+        <h2 className=" h2-bold">
+        Trusted by <br /> Thousands of Event Hosts
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          Cat
+        </div>
+      </section>
+    </>
   );
 }
