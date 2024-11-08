e01656dfc59ce2b9e8c4af90a05a0468283f9a2c
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-08 15:10:49 +0530

    Add feature: footer is added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      Hi from the home page
+      
     </main>
   );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className="border-t">
+      <div className=" flex-center wrapper  flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href={'/'}>
+          <Image src={"/assets/images/logo.svg"}
+          alt ="logo"
+          height={38}
+          width={128}
+          />
+        </Link>
+        <p>2024 Eventers. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
