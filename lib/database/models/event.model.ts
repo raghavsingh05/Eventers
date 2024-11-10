051ba48011c759dc71547622c06388157af10ec6
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-10 19:36:05 +0530

    Complete: DB event model

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,6 +1,20 @@
-import { model, models, Schema } from "mongoose";
+import { Document, model, models, Schema } from "mongoose";
 
-
+export interface IEvent extends Document{
+    _id:string
+    title: string;
+    description?: string;
+    location?:string;
+    createdAt:Date;
+    imageUrl:string;
+    startDateTime:Date;
+    endDateTime:Date;
+    price?:string;
+    isFree: boolean;
+    url?:string;
+    category: { _id:string, name:string}
+    organizer: {_id:string, firstName:string, lastName:string }
+}
 
 const EventsSchema = new Schema({
     title: {type: String, required:true},
