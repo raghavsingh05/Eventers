8a645606efe6a32475f7e665375958e29d5444e0
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-16 13:15:01 +0530

    make: webhook public

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks(.*)` route is not protected here
-export default clerkMiddleware()
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
@@ -9,5 +8,9 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    // Make "/" endpoint public
+    '/',
+    // Make "/api/webhooks" endpoint public
+    '/api/webhooks(.*)',
   ],
-}
+};
