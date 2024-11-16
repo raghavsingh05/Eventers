2defc8f719e9845455dbb38e3b5db89acbd4068f
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-16 21:28:24 +0530

    minot fix: route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -52,7 +52,7 @@ export async function POST(req: Request) {
   const { id } = evt.data
   const eventType = evt.type
   
-  if(eventType === "user.created"){
+  if(eventType === 'user.created'){
     const { id, email_addresses, image_url, first_name, last_name, username} = evt.data;
 
     const user={
