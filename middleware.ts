4369571ddcb659c1bc6930bd09e525c6c28000dd
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-16 14:06:38 +0530

    update: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-    // Make "/" endpoint public
-    '/',
-    // Make "/api/webhooks" endpoint public
-    '/api/webhooks/clerk',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
