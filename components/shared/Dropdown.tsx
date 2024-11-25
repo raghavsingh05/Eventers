2b6c76a2353d91dcfff6b7e948209c0d8c15f47b
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-25 20:32:21 +0530

    add: dropdown menu in create event page

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { ICategory } from '@/lib/database/models/category.model'
+
+
+type DropdownProps = {
+    value?: string
+    onChangeHandler?: () => void
+}
+
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+    const [categories, useCategories] = useState<ICategory[]>([])
+    return (
+        <Select>
+            <SelectTrigger className="select-field">
+                <SelectValue placeholder="Category" />
+            </SelectTrigger>
+            <SelectContent>
+                {categories.length > 0 && categories.map((category) =>(
+                    <SelectItem key={category._id} value='category._id' className='select.item p-regular-14'>
+                        {category.name}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    )
+}
+
+export default Dropdown
