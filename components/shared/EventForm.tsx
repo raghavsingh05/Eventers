690f93085c900a54124bfd4fd45a88a9efad49d7
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-26 23:37:22 +0530

    add: upload drop feature

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -9,6 +9,8 @@ import { z } from "zod"
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "./Dropdown"
 import { Textarea } from "@/components/ui/textarea"
+import FileUploader from "./FileUploader"
+import { useState } from "react"
 
 
 type EventFormProps = {
@@ -17,6 +19,7 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+    const [files , setFiles] = useState<File[]>([])
     const initialValues = eventDefaultValues;
     const form = useForm<z.infer<typeof EventformSchema>>({
         resolver: zodResolver(EventformSchema),
@@ -61,7 +64,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         name="description"
                         render={({ field }) => (
                             <FormItem className="w-full">
-                                <FormControl className="h-56">
+                                <FormControl className="h-72">
                                     <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
                                 </FormControl >
                                 <FormMessage />
@@ -73,8 +76,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         name="imageUrl"
                         render={({ field }) => (
                             <FormItem className="w-full">
-                                <FormControl className="h-56">
-                                    
+                                <FormControl className="h-72">
+                                    <FileUploader onFieldChange={field.onChange} imageUrl={field.value} setFiles={setFiles}/>
                                 </FormControl >
                                 <FormMessage />
                             </FormItem>
