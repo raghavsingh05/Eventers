8644aa7d5a281c972cdc19cea4ba2568995a17ed
raghavsingh05 <raghavsingh47336@gmail.com>
2024-11-30 11:24:59 +0530

    add: event page completed

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,11 +3,11 @@ import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
 import Image from 'next/image';
 
-const EventDetails = async({params: {id}}: SearchParamProps) =>{
-    const event  = await getEventById(id);
-    return(
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+    const event = await getEventById(id);
+    return (
         <section className='flex justify-center bg-primary-50 bg-dotted-pattern'>
-            <div className='grid grid-col-1 md:grid-cols-2 2xl:max-w-7xl'> 
+            <div className='grid grid-col-1 md:grid-cols-2 2xl:max-w-7xl'>
                 <Image
                     src={event.imageUrl}
                     alt='hero image'
@@ -21,7 +21,7 @@ const EventDetails = async({params: {id}}: SearchParamProps) =>{
                         <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
                             <div className='flex gap-3'>
                                 <p className='p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700'>
-                                    {event.isFree? 'FREE':`₹${event.price}` }
+                                    {event.isFree ? 'FREE' : `₹${event.price}`}
                                 </p>
                                 <p className='p-medium-16 rounded-full bg-green-500/10 px-4 py-2.5 text-green-500'>
                                     {event.category.name}
@@ -35,14 +35,29 @@ const EventDetails = async({params: {id}}: SearchParamProps) =>{
                     <div className='flex flex-col gap-5'>
                         <div className='flex gap-2 md:gap-3'>
                             <Image src="/assets/icons/calendar.svg" alt='calendar'
-                            width={32}
-                            height={32} />
+                                width={32}
+                                height={32} />
                             <div className='p-medium-16 lg:p-regular-20 flex flex-wrap items-center'>
-                                <p>{formatDateTime(event.startDateTime).dateOnly} /</p>
-                                <p className='ml-1'>{formatDateTime(event.startDateTime).timeOnly} - {' '}</p>
-                                <p className='ml-1'>{formatDateTime(event.endDateTime).timeOnly} {' '}</p>
+                                <p className='mr-1'>
+                                    {formatDateTime(event.startDateTime).dateOnly} - {' '}
+                                    {formatDateTime(event.startDateTime).timeOnly} /
+                                </p>
+                                <p>
+                                    {formatDateTime(event.endDateTime).dateOnly} - {' '}
+                                    {formatDateTime(event.endDateTime).timeOnly} 
+                                </p>
                             </div>
                         </div>
+                        <div className='p-regular-20 flex items-center gap-3'>
+                            <Image src="/assets/icons/location.svg" alt='location' width={32} height={32} />
+                            <p className='p-medium-16 lg:p-regular-20'>{event.location}</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <p className='p-bold-20 text-grey-600'>What you'll learn: </p>
+                        <p className='p-medium-16 lg:p-regular-18'>{event.description}</p>
+                        <p className='p-medium-16 lg:p-regular-18 truncate text-primary-500 underline'>{event.url}</p>
+
                     </div>
                 </div>
             </div>
